c9eb827ade8afdd94f78cdce920a86eac4a3f18f
tomrowland-dev <tom.rowland@xavier.vic.edu.au>
2022-12-08 11:04:37 +1100

    Initial commit of header components

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./header.css"
+
+const Header = () => {
+    return (
+        <div>
+            <div className='header-css'>
+                <h1>{"<"}Tom R/{">"}</h1>
+                <p className='flav-text'> 16y/o hobby dev</p>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
