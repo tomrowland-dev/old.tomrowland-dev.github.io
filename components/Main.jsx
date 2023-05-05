222de5e4f0601b051bd7b0504701dcfb62530aa7
tomrowland-dev <44159624+tomrowland-dev@users.noreply.github.com>
2023-05-05 02:41:20 +0000

    Deploying to gh-pages from @ tomrowland-dev/tomrowland-dev.github.io@6c10f3260822fa9d2390e3e7b04bc058b9196c6e 🚀

---
+++ components/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./main.css"
+
+const Main = () => {
+    return (
+        <div>
+            <div className='scale-in-center'>
+
+
+                <div className='content-main'>
+                    <h1>Hi, I'm Tom </h1>
+                    <p> Website is a WIP come back another time :)</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main
