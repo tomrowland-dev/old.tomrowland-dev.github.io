4e2defea03929cd52ae7e4cbd7c282ba6d6c5e7c
tomrowland-dev <tom.rowland@xavier.vic.edu.au>
2023-05-09 13:47:31 +1000

    Fixed the about link.. react router moment

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import "./main.css"
+import About from './AboutPage/About'
+import { Link } from 'react-router-dom'
 
 const Main = () => {
     return (
@@ -10,7 +12,7 @@ const Main = () => {
                 <div className='content-main'>
                     <h1 className='main-text'>Hi, I'm Tom</h1>
                     <p>A 17y/o Self taught developer from Melbourne</p>
-                    <button> About </button>
+                    <Link to="/about"><button> About </button></Link>
                 </div>
             </div>
         </div>
